824255925be12734aa25213d41f4207f4bd282c7
NTshabalala-Pears <nhlanhla.tshabalala@wearepears.co.za>
2024-10-16 19:59:04 +0200

    test 3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
                 <h2>
                     Environment: {process.env.REACT_APP_ENV}
                 </h2>
+                <h1>
+                    Testing git flow
+                </h1>
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
         </div>
