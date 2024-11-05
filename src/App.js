319ead39d35251b4b17d7cff9a1dd2d8c3e87be5
NTshabalala-Pears <nhlanhla.tshabalala@wearepears.co.za>
2024-11-05 20:22:30 +0200

    test flow

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>
-                    {process.env.REACT_APP_NAME} landing page coming soon
+                    {process.env.REACT_APP_NAME} landing page coming soon. very soon
                 </h1>
                 <h2>
                 </h2>
