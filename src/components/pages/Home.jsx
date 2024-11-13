d49ec3b9c361ef7ffa66b7ee25011e824f333339
Gresham24 <tembogresham@gmail.com>
2024-11-13 23:51:14 +0200

    created homepage component

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            Inspired by nature, driven by technology, perfected in partnership. Welcome to Pears.
+        </div>
+    );
+}
+
+export default Home;
