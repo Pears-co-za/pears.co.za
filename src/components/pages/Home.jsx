6656aa9cd51562dc3eadaa5b0dd6bc0425c2a921
Gresham24 <tembogresham@gmail.com>
2025-04-02 21:00:29 +0200

    navbar, hero, footer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,51 +1,22 @@
-import { ButtonCTA } from "../common/ButtonCTA";
-import Accordion from "../common/Accordion";
-import FadeIn from "../common/FadeIn";
-import {
-    StyledHightlightText,
-    StyledDisplayHeading,
-    StyledHomeContainer,
-    Styled3ColSection,
-    StyledH1,
-    StyledServicesSection,
-} from "../../styles/Home.styles";
+import React from "react";
+import Hero from "../layout/Hero";
+import { StyledHomeContainer } from "../../styles/Home.styles";
 
 function Home() {
     return (
-        <StyledHomeContainer>
-            <div>
-                <FadeIn>
-                    <StyledDisplayHeading>
-                    Heeloooooo.{" "}
-                        <StyledHightlightText>
-                            Welcome to Pears.
-                        </StyledHightlightText>
-                    </StyledDisplayHeading>
-                </FadeIn>
-
-                <FadeIn delay={200}>
-                    <Styled3ColSection>
-                        <p>Our Story</p>
-                        <StyledH1>
-                            We're a team of IT specialists inspired by nature's
-                            harmony.
-                            <StyledHightlightText>
-                                {" "}
-                                Let us help you reach new heights.
-                            </StyledHightlightText>
-                        </StyledH1>
-                        <ButtonCTA to="/about">About Us</ButtonCTA>
-                    </Styled3ColSection>
-                </FadeIn>
-
-                <FadeIn delay={400}>
-                    <StyledServicesSection>
-                        <p>Services</p>
-                        <Accordion />
-                    </StyledServicesSection>
-                </FadeIn>
-            </div>
-        </StyledHomeContainer>
+        <>
+            <Hero 
+                backgroundImage="/assets/home-hero-bg.png"
+                title="Inspired by nature, driven by technology, perfected in partnership. Welcome to Pears."
+                buttonText="Learn About Us"
+                buttonLink="/about"
+                fullHeight={true}
+            />
+            
+            <StyledHomeContainer>
+                {/* Rest of the content will be added later */}
+            </StyledHomeContainer>
+        </>
     );
 }
 
--- src/styles/Home.styles.js
+++ src/styles/Home.styles.js
@@ -2,29 +2,15 @@ import styled from "styled-components";
 import { colors } from "../constants";
 
 export const StyledHomeContainer = styled.div`
-    padding: 2rem 4rem;
+    padding: 4rem;
     max-width: 1280px;
     margin: 0 auto;
     @media (max-width: 768px) {
-        padding: 1.5rem 2rem;
+        padding: 3rem 2rem;
     }
 
     @media (max-width: 480px) {
-        padding: 1rem;
-    }
-`;
-
-export const StyledDisplayHeading = styled.h1`
-    font-size: 4rem;
-    font-weight: 500;
-    line-height: 1.2;
-    @media (max-width: 768px) {
-        font-size: 3rem;
-    }
-
-    @media (max-width: 480px) {
-        font-size: 2.5rem;
-        text-align: left;
+        padding: 2rem 1rem;
     }
 `;
 
